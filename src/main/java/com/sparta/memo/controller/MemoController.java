ab3bc00def900ba56bfba5deb11a0e383a67386b
tktcod <hs060100@gmail.com>
2024-01-23 10:14:40 +0900

    memo project (not include mysql password)

---
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -0,0 +1,40 @@
+package com.sparta.memo.controller;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.service.MemoService;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final MemoService memoService;
+
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
+    }
+
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+        return memoService.createMemo(requestDto);
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+        return memoService.getMemos();
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        return memoService.updateMemo(id, requestDto);
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        return memoService.deleteMemo(id);
+    }
+}
