1eaea85b6d0e21daced19c6c524b8cd9b5046296
tktcod <hs060100@gmail.com>
2024-01-23 11:19:37 +0900

    query Method

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -4,7 +4,9 @@ import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
